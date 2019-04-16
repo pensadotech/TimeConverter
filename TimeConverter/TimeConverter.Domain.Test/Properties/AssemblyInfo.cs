0666cfa1584a15362ff87dc0f9728f3334d24a3f
pensadotech <armando@pensadotech.com>
2019-04-15 20:01:53 -0700

    add TimeConvertionFunctionsTest

---
+++ TimeConverter/TimeConverter.Domain.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TimeConverter.Domain.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TimeConverter.Domain.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("86ef34cc-d6d3-4da8-80ca-e817f33a323f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TimeConverter/TimeConverter.Domain.Test/TimeConvertionFunctionsTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeConverter.Domain.Functionality;
+using TimeConverter.Domain.Interfaces.Repositories;
+
+namespace TimeConverter.Domain.Test
+{
+    [TestClass]
+    public class TimeConvertionFunctionsTest
+    {
+        private ITimeConverterRepository _timeConvrtRepo; 
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Create a Time convertion repo
+            _timeConvrtRepo  = new TimeConvertionFunctions();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            // nothing
+        }
+
+        [TestMethod]
+        public void ConvertDate12HrTimeToSecondsTest()
+        {
+            // Arrange
+            double refSeconds = 44614.235;
+            // use current date staring from midnight today andd add seconds 
+            DateTime targetDateTime = DateTime.Today.AddSeconds(refSeconds);
+
+            // Act
+            double resultSecs = _timeConvrtRepo.ConvertDate12HrTimeToSeconds(targetDateTime);
+
+            // Assert
+            Assert.AreEqual(refSeconds, resultSecs);
+        }
+
+        [TestMethod]
+        public void ConvertSecondsToCurrentDateTimeTest()
+        {
+            // Arrange
+            double refSeconds = 44614.235;
+            DateTime refDateTime = DateTime.Today.AddSeconds(refSeconds);
+
+            // Act
+            DateTime resultDateTime = _timeConvrtRepo.ConvertSecondsToCurrentDateTime(refSeconds);
+
+            // Assert 
+            Assert.AreEqual(refDateTime, resultDateTime);
+        }
+
+        [TestMethod]
+        public void ConvertString24HrTimeToSeconds()
+        {
+            // Arrange
+            // Arrange
+            double refSeconds = 44614.235;
+            string target24Time = "12:23:34.235";
+
+            // Act
+            double resultSecs = _timeConvrtRepo.ConvertString24HrTimeToSeconds(target24Time);
+
+            // Assert
+            Assert.AreEqual(refSeconds, resultSecs);
+        }
+    }
+}
